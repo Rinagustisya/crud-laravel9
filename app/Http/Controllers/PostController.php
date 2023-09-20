1d52f063dbafa0ea4fa1480dd50d1120c78b84ba
Rinagustisya <rinaagustisya885@gmail.com>
2023-09-20 14:43:39 +0700

    feat  : add method store

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Http\RedirectResponse;
 
 class PostController extends Controller
 {
@@ -18,10 +19,30 @@ class PostController extends Controller
 
     public function create()
     {
-        // get posts
-        $posts = Post::latest()->paginate(5);
-
         // render view with post
-        return view('posts.create', compact('posts'));
+        return view('posts.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        //validate form
+       $this->validate($request, [
+        'image'     => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        'title'     => 'required|min:5',
+        'content'   => 'required|min:10'
+       ]);
+
+       //upload image
+       $image = $request->file('image');
+       $image->storeAs('public/posts', $image->hashName());
+
+       // create
+       Post::create([
+            'image'     => $image->hashName(),
+            'title'     => $request->title,
+            'content'   => $request->content
+       ]);
+
+       return redirect()->route('posts.index')->with(['success'=>'Data berhasil disimpan!']);
     }
 }
