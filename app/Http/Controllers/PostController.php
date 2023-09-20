50dcd1bc6d51cf2cb62ce45911b8ca43291fadfc
Rinagustisya <rinaagustisya885@gmail.com>
2023-09-20 12:22:51 +0700

    feat : add model & controller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        // get posts
+        $posts = Post::latest()->paginate(5);
+
+        // render view with post
+        return view('posts.index', compact('posts'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('/posts', \App\Http\Controller\PostController::class);
