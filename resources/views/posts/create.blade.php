85d3d69849c43a42cd39d1d5dd15c977794249af
Rinagustisya <rinaagustisya885@gmail.com>
2023-09-20 14:45:07 +0700

    fix : bug store

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -12,7 +12,7 @@
         <div class="col-md-12">
         <div class="card border-8 shadow rounded">
         <div class="card-body">
-            <form action="{{ route('post.store') }}" method="POST" enctype="multipart/form-data">
+            <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="form-grub">
                     <label class="font-weight-bold">GAMBAR</label>
